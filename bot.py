f144c008f3147f76b7ef63ec5aab0d598cfaaec1
Anzar299 <anzar@rumahatsiri.com>
2026-01-10 15:26:59 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
     print("Bot started...")
     app.run_polling()
 
