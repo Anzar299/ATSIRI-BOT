47fb37d36763e5f8647563ce156d96bbed47ba3b
Anzar299 <anzar@rumahatsiri.com>
2026-01-10 15:18:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Halo 👋 ATSIRI BOT sudah aktif!")
+    await update.message.reply_text("Bot ATSIRI aktif ✅")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = Application.builder().token(TOKEN).build()
 
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
