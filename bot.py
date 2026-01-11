20cd849aa2dea17b721ed9c58ef401154f8801a6
Anzar299 <anzar@rumahatsiri.com>
2026-01-11 22:48:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ DILUTION_KEYWORDS = [
 DILUTION_RESPONSES = [
     (
         "Halo Aromates 🌿\n\n"
-        "Dilution adalah proses pencampuran minyak Atsiri dengan minyak pembawa atau Carrier Oil\n"
+        "Dilution adalah proses pencampuran minyak Atsiri dengan minyak pembawa atau Carrier Oil "
         "sebelum diaplikasikan ke kulit 💧✨"
     ),
 ]
